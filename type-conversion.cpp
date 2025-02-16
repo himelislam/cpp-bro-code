a2bb099e5def73f0d8a40aab8f21794a93cd9d1b
Himel Islam <himel7100@gmail.com>
2025-02-16 23:33:23 +0600

    conversion

--- type-conversion.cpp
+++ type-conversion.cpp
@@ -3,5 +3,14 @@
 int main(){
     // issue fixed
     // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
+    // issue fixed for linear
     return 0;
 }
